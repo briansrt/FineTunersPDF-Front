91c9e63749e0f111c61c4a111a44c263979b4063
BrayanRM23 <brayan.rodriguez04@unicatolica.edu.co>
2024-12-11 02:16:23 -0500

    prueba2

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -47,7 +47,7 @@ export default function FileUpload() {
     formData.append('phoneNumber', `${countryCode}${phoneNumber}`);
 
     try {
-      const response = await fetch('http://localhost:4000/api/process-document', {
+      const response = await fetch('https://finetunerspdf-back.vercel.app/api/process-document', {
         method: 'POST',
         body: formData,
       });
